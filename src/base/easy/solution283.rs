99ff2f5ec05ce99cdf82aee695ce27beb55e02fd
Nikita Ugnich <nikitagavrilin005@gmail.com>
2026-08-17 18:12:56 +0300

    solution 283

--- src/base/easy/solution283.rs
+++ src/base/easy/solution283.rs
@@ -2,10 +2,22 @@ use crate::base::Solution;
 
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
-        let mut cursor_iter = 0;
-        let mut cursor_nonzero = 0;
+        let mut cursor = 0;
         let mut zeroes_count = 0;
 
-        todo!()
+        while cursor < nums.len() {
+            let value = nums[cursor];
+
+            if value == 0 {
+                nums.remove(cursor);
+                zeroes_count += 1;
+            } else {
+                cursor += 1;
+            }
+        }
+
+        for _ in 1..=zeroes_count {
+            nums.push(0);
+        }
     }
 }
